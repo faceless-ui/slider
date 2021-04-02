39941cfb502289d41fdade51d2cf2d86bb458284
Jacob Fletcher <jacobsfletch@gmail.com>
2021-04-02 12:31:09 -0400

    deprecates production webpack, builds with tsc

--- src/SliderButton/index.tsx
+++ src/SliderButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback } from 'react';
 import useSlider from '../useSlider';
 import { Props } from './types';
 
